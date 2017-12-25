07440056eb7cec2f8160777dc92230acace2c5a2
okaduki <okaduki1@gmail.com>
2017-12-26 00:56:39 +0900

    Modify compile command

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ -isystem ${GTEST_DIR}/include -pthread main.cpp libgtest.a -o travis_test
+g++ -isystem ${GTEST_DIR}/include -pthread main.cpp ${GTEST_DIR}/libgtest.a -o travis_test
